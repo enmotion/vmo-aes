0da78c3bda31d65a3e926c446d7f8150be1bc7dd
enmotion <40427163@qq.com>
2024-06-12 20:58:37 +0800

    init

---
+++ index.ts
@@ -0,0 +1,6 @@
+import VmoButton from "@comps/vmo-button/vmo-button.vue";
+import type { ButtonParams } from "@comps/vmo-button/types";
+export {
+  VmoButton,
+  ButtonParams
+}
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { VmoButton } from "../index"
+</script>
+
+<template>
+  <div class="flex flex-col flex-grow items-center justify-center">
+    <vmo-button perfix="ssee">aaaa</vmo-button>
+  </div>
+</template>
+
+<style>
+html,body{
+    height: 100%;
+    display: flex;
+    flex-grow: 1;
+}
+</style>
